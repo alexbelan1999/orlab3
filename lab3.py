9f5c05a67b21b9fac925dffa26d6e8a6bc4913a9
alexbelan1999 <alexbelan1999@gmail.com>
2019-10-19 18:37:28 +0300

    Add FordFalk method

--- lab3.py
+++ lab3.py
@@ -2,9 +2,10 @@ import math
 import numpy as np
 import networkx as nx
 import matplotlib.pyplot as plt
-
+import func
 nodes = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-edges_with_weight = [(1,2,10), (1,4,5), (2,3,3), (2,5,9) , (3,6,11), (4,2,5), (4,5,10), (4,7,6), (4,8,15), (5, 6,13), (5,8,10), (5,9,5), (6,9,7), (7,8,8), (8,9,4)]
+edges_with_weight = [[1,2,10], [1,4,5], [2,3,3], [2,5,9] , [3,6,11], [4,2,5], [4,5,10], [4,7,6], [4,8,15], [5, 6,13], [5,8,10], [5,9,5], [6,9,7], [7,8,8], [8,9,4]]
+edges_with_weight1 = [(1,2,10), (1,4,5), (2,3,3), (2,5,9) , (3,6,11), (4,2,5), (4,5,10), (4,7,6), (4,8,15), (5, 6,13), (5,8,10), (5,9,5), (6,9,7), (7,8,8), (8,9,4)]
 G = nx.DiGraph()
 G.add_nodes_from(nodes)
 G.add_weighted_edges_from(edges_with_weight)
@@ -14,3 +15,5 @@ nx.draw(G,layout,with_labels=True,node_color='b')
 labels = nx.get_edge_attributes(G, "weight")
 nx.draw_networkx_edge_labels(G,pos=layout, edge_labels=labels)
 plt.show()
+
+func.FordFalk(nodes,edges_with_weight,edges_with_weight1)
